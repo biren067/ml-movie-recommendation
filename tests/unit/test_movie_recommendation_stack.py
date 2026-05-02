ae55fee473a206594596e05a11b2bcf349e781dc
biren067 <birendrabitian@gmail.com>
2026-05-02 17:48:46 +0530

    Initial commit

---
+++ tests/unit/test_movie_recommendation_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from movie_recommendation.movie_recommendation_stack import MovieRecommendationStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in movie_recommendation/movie_recommendation_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = MovieRecommendationStack(app, "movie-recommendation")
+    template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
